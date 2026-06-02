311221e3f21deda56b7a056567596a3c33ac3aae
Anoop <006shido@gmail.com>
2026-06-02 23:40:25 +0530

    Add input prompt for guessing attempts

--- index.c
+++ index.c
@@ -10,3 +10,7 @@ int main() {
 
     printf("=== Number Guessing Game ===\n");
     printf("Guess a number between 1 and 100\n");
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
